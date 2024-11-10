1112fbe0b2b4ef56a2a582cf2e878f6708c66893
T45K <tasktas9@gmail.com>
2024-11-10 22:29:32 +0900

    delete old fiels

--- src/main/kotlin/io/github/t45k/feature_flag_remover/internal/removal.kt
+++ src/main/kotlin/io/github/t45k/feature_flag_remover/internal/removal.kt
@@ -1,17 +1,29 @@
 package io.github.t45k.feature_flag_remover.internal
 
-import io.github.t45k.feature_flag_remover.internal.ast.removeElseClauseTarget.IfExpressionWithRemoveElseClauseAfterReleaseAnnotationAstNode
-import io.github.t45k.feature_flag_remover.internal.ast.removeTarget.RemoveCandidateAstNode
+import org.jetbrains.kotlin.psi.KtBlockExpression
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtIfExpression
+import org.jetbrains.kotlin.psi.psiUtil.endOffset
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
-fun removeFromText(
+internal fun removeFromText(
     text: String,
-    removeTargets: List<RemoveCandidateAstNode>,
-    removeElseClauseTargets: List<IfExpressionWithRemoveElseClauseAfterReleaseAnnotationAstNode>,
+    removeTargets: List<KtElement>,
+    removeElseClauseTargets: List<KtIfExpression>,
 ): String = text.filterIndexed { index, c ->
-    val isIndexInRemoveTarget = removeTargets.any { index in it.sourceRange }
-    val isIndexOfTailingCommaOfRemoveTarget = c == ',' && removeTargets.any { index == (it.sourceRange.last + 1) }
-    val isIndexInRemoveElseClauseTarget = removeElseClauseTargets.any { index in it.wholeExpressionSourceRange }
-    val isIndexInThenClauseOfRemoveElseClauseTarget = removeElseClauseTargets.any { index in it.thenClauseSourceRange }
+    val isIndexInRemoveTarget = removeTargets.any { index in it.startOffset..<it.endOffset }
+    val isIndexOfTailingCommaOfRemoveTarget = c == ',' && removeTargets.any { index == it.endOffset }
+    val isIndexInRemoveElseClauseTarget = removeElseClauseTargets.any { index in it.parent.startOffset..<it.parent.endOffset }
+    val isIndexInThenClauseOfRemoveElseClauseTarget = removeElseClauseTargets.any {
+        when (val thenExpression = it.then) {
+            is KtBlockExpression ->
+                if (thenExpression.statements.isEmpty()) return@any false
+                else index in thenExpression.statements.first().startOffset..<thenExpression.statements.last().endOffset
+
+            null -> return@any false
+            else -> index in thenExpression.startOffset..<thenExpression.endOffset
+        }
+    }
 
     val isRemoveTargetChar =
         isIndexInRemoveTarget ||
