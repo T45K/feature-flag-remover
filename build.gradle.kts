5e27a8fc27e980363b7687a4e24354e681539c2b
T45K <tasktas9@gmail.com>
2024-11-10 22:30:16 +0900

    remove deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.kotlinx.ast:grammar-kotlin-parser-antlr-kotlin:v0.1.0")
     implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
