aefa846c122523c3c890c73e4bae4116bc2348a6
T45K <tasktas9@gmail.com>
2025-06-29 12:41:26 +0900

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ subprojects {
     }
 
     group = "com.github.t45k"
-    version = "0.1.2"
+    version = "1.0.0"
 
     dependencies {
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
