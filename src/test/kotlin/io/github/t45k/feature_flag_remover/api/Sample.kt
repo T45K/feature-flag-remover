041c1480734704c6772b56e43cc6a5767368233a
T45K <tasktas9@gmail.com>
2025-11-03 10:45:42 +0900

    add Sample code in test module

--- src/test/kotlin/io/github/t45k/feature_flag_remover/api/Sample.kt
+++ src/test/kotlin/io/github/t45k/feature_flag_remover/api/Sample.kt
@@ -1,8 +1,22 @@
+@file:Suppress(
+    "unused",
+    "KotlinConstantConditions",
+    "UnusedVariable",
+    "ConstantConditionIf",
+    "ANNOTATION_WILL_BE_APPLIED_ALSO_TO_PROPERTY_OR_FIELD",
+    "USELESS_IS_CHECK",
+    "REDUNDANT_ELSE_IN_WHEN",
+)
+
 package io.github.t45k.feature_flag_remover.api
 
 class Sample(
     @RemoveAfterRelease("sample") // property
     private val sample: String,
+    @RemoveAfterRelease("sample") // property with initial value
+    private val sample2: String = "",
+    @param:RemoveAfterRelease("sample") // property with use-sites
+    private val sample3: String = "",
 ) {
     fun sample() {
         @RemoveAfterRelease("sample") // expression statement
@@ -11,12 +25,14 @@ class Sample(
         @RemoveAfterRelease("sample") // local variable statement,
         val a = "hogehoge"
 
-        val list = listOf(
+        listOf(
             @RemoveAfterRelease("sample") // value argument
             true
         )
 
-        val sample = Sample(@RemoveAfterRelease("sample") sample)
+        listOf(element = @RemoveAfterRelease("sample") true) // named argument
+
+        val sample = Sample(@RemoveAfterRelease("sample") "sample") // constructor argument
 
         val b = @RemoveElseClauseAfterRelease("sample") if (true) {
             "enabled"
@@ -25,6 +41,33 @@ class Sample(
         }
 
         val c = @RemoveElseClauseAfterRelease("sample") if (true) "enabled" else "disabled"
+
+        // conditions in when expression
+        val d = when ("a") {
+            @RemoveAfterRelease("sample") "a" -> "a"
+            else -> "b"
+        }
+
+        val e = when ("a") {
+            is String -> @RemoveAfterRelease("sample") "a"
+            else -> "b"
+        }
+
+        val f = when ("a") {
+            is String -> @RemoveAfterRelease("sample") {
+                "a"
+            }
+
+            else -> "b"
+        }
+
+        val g = when ("a") {
+            is String -> {
+                @RemoveAfterRelease("sample") "a"
+            }
+
+            else -> "b"
+        }
     }
 
     @RemoveAfterRelease("sample")
